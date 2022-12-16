5e71646d29bb1ba2be5e1bef0977c65ad9d311ac
FonarevaOlga <oefonareva@edu.hse.ru>
2022-12-16 17:12:29 +0300

    Add ex21

---
+++ ex21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите координаты первого числа:");
+Console.Write("x1: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1: ");
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второго числа:");
+Console.Write("x2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2: ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2)+Math.Pow(y1 - y2, 2)+Math.Pow(z1 - z2, 2)),2)}");
