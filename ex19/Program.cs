53fb12dbbfb0203beb4a7525204459ad2e36a420
FonarevaOlga <oefonareva@edu.hse.ru>
2022-12-16 16:57:19 +0300

    Add ex19

---
+++ ex19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Ведите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine()); //12345
+
+if (number / 10000 == number % 10) {
+    if ((number % 100) /10 == (number % 10000) / 1000) {
+        Console.WriteLine($"Число {number} является полиндромом");
+    } else {Console.WriteLine($"Число {number} не является полиндромом");}
+} else {Console.WriteLine($"Число {number} не является полиндромом");}
